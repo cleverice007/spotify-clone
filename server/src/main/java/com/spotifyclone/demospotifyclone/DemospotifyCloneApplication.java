1059065cf7b3ab23e2291ae21c809f3580f3ce42
cleverice007 <cleverice90128@gmail.com>
2023-10-07 23:42:34 +0800

    環境設定 frontend ts & backend JAVA

---
+++ server/src/main/java/com/spotifyclone/demospotifyclone/DemospotifyCloneApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyclone.demospotifyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospotifyCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospotifyCloneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/spotifyclone/demospotifyclone/DemospotifyCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifyclone.demospotifyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospotifyCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
