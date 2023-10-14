6d17676c0618ba90be692056887cc98ddc21b78a
cleverice007 <cleverice90128@gmail.com>
2023-10-14 13:06:34 +0800

    album gallery 的播放邏輯移到home

--- client/src/components/AlbumGallery.tsx
+++ client/src/components/AlbumGallery.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Album,Song } from '../interfaces/MusicModels';
+import { Album, Song } from '../interfaces/MusicModels';
 import { getAllAlbums, getSongsInAlbum } from '../services/musicService'; 
-import PlaybackBar from './PlaybackBar';
 
 const AlbumGallery: React.FC = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
   const [currentSong, setCurrentSong] = useState<Song | null>(null); 
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   
-
   useEffect(() => {
     const fetchAlbums = async () => {
       const fetchedAlbums = await getAllAlbums();
@@ -17,23 +15,27 @@ const AlbumGallery: React.FC = () => {
 
     fetchAlbums();
   }, []);
-  const handleAlbumClick = async (albumId: string, albumCoverUrl: string) => {
+
+  const handleAlbumClick = async (albumId: string) => {
     const songs = await getSongsInAlbum(albumId);
-    if (songs.length > 0) { // 確保專輯裡有歌曲
-        setCurrentSong(songs[0]);
-        setIsPlaying(true); // 開始播放
+    if (songs.length > 0) { 
+      setCurrentSong(songs[0]);
+      setIsPlaying(true);
     }
   };
 
   return (
     <div className="flex flex-wrap gap-4">
       {albums.map(album => (
-        <div key={album.id} className="w-40 text-center" onClick={() => handleAlbumClick(album.id, album.coverUrl)}>
+        <div 
+          key={album.id} 
+          className="w-40 text-center" 
+          onClick={() => handleAlbumClick(album.id)}
+        >
           <img src={album.coverUrl} alt={album.title} className="w-full rounded-md" />
           <div>{album.title}</div>
         </div>
       ))}
-      <PlaybackBar currentSong={currentSong} isPlaying={isPlaying} onPlayPause={() => setIsPlaying(!isPlaying)} albums={albums} />
     </div>
   );
 };
