76404aa81ce056b26fb7fedfc098616ad1f81bdb
cleverice007 <cleverice90128@gmail.com>
2023-10-18 12:57:03 +0800

    先製作上傳音樂的前後端

---
+++ server/src/main/java/com/spotifyclone/demospotifyclone/controller/MusicUploadController.java
@@ -0,0 +1,18 @@
+package com.spotifyclone.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.spotifyclone.repo.MusicRepo;
+
+@RestController
+public class MusicUploadController {
+
+    @Autowired
+    MusicRepo musicRepo;
+
+    @PostMapping("/upload")
+    public String uploadMusic(@RequestParam("file") MultipartFile file) {
+        return musicRepo.uploadFileToS3(file);
+    }
+}
