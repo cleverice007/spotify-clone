c25ce5822a9221a656261ca729d7923947484b2c
cleverice007 <cleverice90128@gmail.com>
2023-10-27 18:06:07 +0800

    直接從 musicservice 引入function

--- server/src/main/java/com/spotifyclone/demospotifyclone/service/LambdaService.java
+++ server/src/main/java/com/spotifyclone/demospotifyclone/service/LambdaService.java
@@ -14,34 +14,13 @@ public class LambdaHandlerService {
 
     /**
      * 處理上傳歌曲的Lambda請求
-     *
-     * @param audioFile    音頻文件
-     * @param coverFile    封面圖片
-     * @param albumTitle   專輯名稱
-     * @param songTitle    歌曲標題
-     * @return             上傳結果
      */
     public String handleUploadSong(MultipartFile audioFile, MultipartFile coverFile, String albumTitle, String songTitle) {
-        // 使用MusicService上傳音樂和封面到S3
-        String uploadResult = musicService.uploadFileToS3(audioFile, coverFile, albumTitle, songTitle);
-        if (uploadResult.contains("Error")) {
-            return uploadResult;
-        }
-
-        // 構造S3的檔案路徑
-        String audioPath = albumTitle + "/songs/" + songTitle + ".mp3";
-        String coverPath = albumTitle + "/cover/cover.jpg";
-        
-        // 使用MusicService將歌曲資料上傳到數據庫
-        String dbResult = musicService.uploadSongToDB(null, songTitle, "Unknown Artist", audioPath, coverPath, albumTitle);
-
-        return dbResult;
+        return musicService.uploadFileToS3(audioFile, coverFile, albumTitle, songTitle);
     }
 
     /**
      * 處理獲取所有專輯列表的Lambda請求
-     *
-     * @return 專輯列表
      */
     public List<Album> handleGetAllAlbums() {
         return musicService.getAllAlbums();
