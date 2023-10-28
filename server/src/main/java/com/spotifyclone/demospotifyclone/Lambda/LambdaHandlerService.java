db09b28c5a911f5f54d968afc2883de5a55829b4
cleverice007 <cleverice90128@gmail.com>
2023-10-28 12:04:18 +0800

    上傳音樂、獲取所有專輯分成不同的request handler

--- server/src/main/java/com/spotifyclone/demospotifyclone/Lambda/LambdaHandlerService.java
+++ server/src/main/java/com/spotifyclone/demospotifyclone/Lambda/LambdaHandlerService.java
@@ -6,42 +6,42 @@ import org.springframework.web.multipart.MultipartFile;
 import com.spotifyclone.demospotifyclone.model.Album;
 import com.spotifyclone.demospotifyclone.service.MusicService;
 
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
-public class LambdaHandlerService implements RequestHandler<Map<String, Object>, String> {
+class LambdaHandlers {
 
-    private MusicService musicService = new MusicService();
+    private static MusicService musicService = new MusicService();
 
-    @Override
-    public String handleRequest(Map<String, Object> input, Context context) {
-        // 根據input來判斷應該呼叫哪個方法
-        String action = (String) input.get("action");
-        if ("uploadSong".equals(action)) {
+    public static class UploadSongLambdaHandler implements RequestHandler<Map<String, Object>, String> {
+
+        @Override
+        public String handleRequest(Map<String, Object> input, Context context) {
             // 從input中提取所需的參數，然後調用handleUploadSong方法
-            // 注意：使用AWS Lambda和API Gateway時，MultipartFile可能不適用，而是直接使用byte[]或base64編碼的字符串
-            // 此處僅作為示例
             MultipartFile audioFile = (MultipartFile) input.get("audioFile");
             MultipartFile coverFile = (MultipartFile) input.get("coverFile");
             String albumTitle = (String) input.get("albumTitle");
             String songTitle = (String) input.get("songTitle");
             return handleUploadSong(audioFile, coverFile, albumTitle, songTitle);
-        } else if ("getAllAlbums".equals(action)) {
-            List<Album> albums = handleGetAllAlbums();
-            // 將結果轉換為JSON字符串或其他適當的格式返回
-            // 此處僅作為示例
-            return albums.toString();
-        } else {
-            return "Unsupported action";
+        }
+
+        public String handleUploadSong(MultipartFile audioFile, MultipartFile coverFile, String albumTitle, String songTitle) {
+            return musicService.uploadFileToS3(audioFile, coverFile, albumTitle, songTitle);
         }
     }
 
-    public String handleUploadSong(MultipartFile audioFile, MultipartFile coverFile, String albumTitle, String songTitle) {
-        return musicService.uploadFileToS3(audioFile, coverFile, albumTitle, songTitle);
-    }
+    public static class GetAllAlbumsLambdaHandler implements RequestHandler<Map<String, Object>, String> {
 
-    public List<Album> handleGetAllAlbums() {
-        return musicService.getAllAlbums();
+        @Override
+        public String handleRequest(Map<String, Object> input, Context context) {
+            List<Album> albums = handleGetAllAlbums();
+            // 將結果轉換為JSON字符串或其他適當的格式返回
+            return albums.toString();
+        }
+
+        public List<Album> handleGetAllAlbums() {
+            return musicService.getAllAlbums();
+        }
     }
 }
 
