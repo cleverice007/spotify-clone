71a86b54713bd55a208c3629aa825e666e4626e7
cleverice007 <cleverice90128@gmail.com>
2023-10-31 00:04:39 +0800

    把request handler 改成apiproxyrequestevent

--- sam-spotify-clone/HelloWorldFunction/src/main/java/demospotifyclone/Lambda/LambdaHandlerService.java
+++ sam-spotify-clone/HelloWorldFunction/src/main/java/demospotifyclone/Lambda/LambdaHandlerService.java
@@ -4,60 +4,67 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.spotifyclone.demospotifyclone.model.Album;
 import com.spotifyclone.demospotifyclone.service.MusicService;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import com.google.gson.Gson;
 
-
- class LambdaHandlers {
+class LambdaHandlers {
 
     private static final MusicService musicService = new MusicService();
 
-    // Lambda Handler 用於獲取預先簽名的URL
-    public static class GetPresignedUrlsLambdaHandler implements RequestHandler<Map<String, Object>, Map<String, String>> {
-    
-    @Override
-    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-        String albumTitle = (String) input.get("albumTitle");
-        String songTitle = (String) input.get("title");
-
-        // 獲取歌曲的預先簽名URL
-        String songPresignedUrl = musicService.getPresignedUrlForSongUpload(albumTitle, songTitle);
-        // 獲取封面的預先簽名URL
-        String coverPresignedUrl = musicService.getPresignedUrlForCoverUpload(albumTitle);
-
-        Map<String, String> presignedUrls = new HashMap<>();
-        presignedUrls.put("songPresignedUrl", songPresignedUrl);
-        presignedUrls.put("coverPresignedUrl", coverPresignedUrl);
-
-        return presignedUrls;
-    }
-}
-
-
-    // Lambda Handler 用於將歌曲信息上傳到數據庫
-    public static class SaveSongToDbLambdaHandler implements RequestHandler<Map<String, Object>, String> {
+    public static class GetPresignedUrlsLambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
         @Override
-        public String handleRequest(Map<String, Object> input, Context context) {
-            String songId = (String) input.get("songId");
-            String title = (String) input.get("title");
-            String artist = (String) input.get("artist");
-            String filePath = (String) input.get("filePath");  // 此filePath應該是S3中的文件路徑
-            String albumCoverUrl = (String) input.get("albumCoverUrl");  // 若封面也上傳到S3，此處也應該是S3的URL
-            String albumTitle = (String) input.get("albumTitle");
-            
-            return musicService.uploadSongToDB(songId, title, artist, filePath, albumCoverUrl, albumTitle);
+        public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+            String albumTitle = input.getQueryStringParameters().get("albumTitle");
+            String songTitle = input.getQueryStringParameters().get("title");
+
+            String songPresignedUrl = musicService.getPresignedUrlForSongUpload(albumTitle, songTitle);
+            String coverPresignedUrl = musicService.getPresignedUrlForCoverUpload(albumTitle);
+
+            Map<String, String> presignedUrls = new HashMap<>();
+            presignedUrls.put("songPresignedUrl", songPresignedUrl);
+            presignedUrls.put("coverPresignedUrl", coverPresignedUrl);
+
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+            response.setStatusCode(200);
+            response.setBody(new Gson().toJson(presignedUrls));
+            return response;
         }
     }
 
-
-    public static class GetAllAlbumsLambdaHandler implements RequestHandler<Map<String, Object>, String> {
+    public static class SaveSongToDbLambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
         @Override
-        public String handleRequest(Map<String, Object> input, Context context) {
+        public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+            String songId = input.getQueryStringParameters().get("songId");
+            String title = input.getQueryStringParameters().get("title");
+            String artist = input.getQueryStringParameters().get("artist");
+            String filePath = input.getQueryStringParameters().get("filePath");
+            String albumCoverUrl = input.getQueryStringParameters().get("albumCoverUrl");
+            String albumTitle = input.getQueryStringParameters().get("albumTitle");
+
+            String result = musicService.uploadSongToDB(songId, title, artist, filePath, albumCoverUrl, albumTitle);
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+            response.setStatusCode(200);
+            response.setBody(result);
+            return response;
+        }
+    }
+
+    public static class GetAllAlbumsLambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+        @Override
+        public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
             List<Album> albums = musicService.getAllAlbums();
-            return albums.toString();
+            
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+            response.setStatusCode(200);
+            response.setBody(new Gson().toJson(albums));
+            return response;
         }
     }
 }
