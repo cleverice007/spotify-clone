a337e79ce002c8909018ed15119d6415dc91e9c2
cleverice007 <cleverice90128@gmail.com>
2023-10-31 23:40:56 +0800

    增加song 的假資料，可以測試get all songs

--- sam-spotify-clone/HelloWorldFunction/src/test/LambdaHandlersTest.java
+++ sam-spotify-clone/HelloWorldFunction/src/test/LambdaHandlersTest.java
@@ -3,11 +3,23 @@ package com.spotifyclone.demospotifyclone.Lambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.spotifyclone.demospotifyclone.Lambda.LambdaHandlers.GetAllAlbumsLambdaHandler;
+import com.spotifyclone.demospotifyclone.Lambda.LambdaHandlers.GetPresignedUrlsLambdaHandler;
+import com.spotifyclone.demospotifyclone.Lambda.LambdaHandlers.SaveSongToDbLambdaHandler;
+import com.spotifyclone.demospotifyclone.model.Album;
+import com.spotifyclone.demospotifyclone.model.Song;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import com.spotifyclone.demospotifyclone.repo.AlbumRepository;
+import com.spotifyclone.demospotifyclone.model.Album;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 class LambdaHandlersTest {
 
@@ -29,7 +41,6 @@ class LambdaHandlersTest {
 
         APIGatewayProxyResponseEvent responseEvent = getUrlsHandler.handleRequest(requestEvent, context);
         assertEquals(200, responseEvent.getStatusCode());
-        // 其他斷言可以確認響應的正確性
     }
 
     @Test
@@ -39,16 +50,62 @@ class LambdaHandlersTest {
 
         APIGatewayProxyResponseEvent responseEvent = saveSongHandler.handleRequest(requestEvent, context);
         assertEquals(200, responseEvent.getStatusCode());
-        // 其他斷言可以確認響應的正確性
     }
 
-    @Test
-    void testGetAllAlbums() {
-        APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
-        // 為 getAllAlbums 函數提供必要的請求參數，如果有的話
+@Test
+void testGetAllAlbums() {
+    // 模擬數據
+    Album album1 = new Album(); // 初始化 album1
+    Album album2 = new Album(); // 初始化 album2
 
-        APIGatewayProxyResponseEvent responseEvent = getAllAlbumsHandler.handleRequest(requestEvent, context);
-        assertEquals(200, responseEvent.getStatusCode());
-        // 其他斷言可以確認響應的正確性
-    }
+    // 創建 album1 的兩首歌
+    Song song1 = new Song();
+    song1.setId("1");
+    song1.setTitle("Song1");
+    song1.setArtist("Artist1");
+    song1.setDuration(300);
+    song1.setFilePath("/path/to/file1");
+    song1.setAlbumCoverUrl("/path/to/cover1");
+    
+    Song song2 = new Song();
+    song2.setId("2");
+    song2.setTitle("Song2");
+    song2.setArtist("Artist2");
+    song2.setDuration(320);
+    song2.setFilePath("/path/to/file2");
+    song2.setAlbumCoverUrl("/path/to/cover2");
+    
+    album1.setSongs(Arrays.asList(song1, song2));
+
+    // 創建 album2 的兩首歌
+    Song song3 = new Song();
+    song3.setId("3");
+    song3.setTitle("Song3");
+    song3.setArtist("Artist3");
+    song3.setDuration(310);
+    song3.setFilePath("/path/to/file3");
+    song3.setAlbumCoverUrl("/path/to/cover3");
+    
+    Song song4 = new Song();
+    song4.setId("4");
+    song4.setTitle("Song4");
+    song4.setArtist("Artist4");
+    song4.setDuration(330);
+    song4.setFilePath("/path/to/file4");
+    song4.setAlbumCoverUrl("/path/to/cover4");
+    
+    album2.setSongs(Arrays.asList(song3, song4));
+
+    // 模擬 albumRepository 的行為
+    List<Album> mockAlbums = Arrays.asList(album1, album2);
+    when(albumRepository.findAll()).thenReturn(mockAlbums);
+
+    APIGatewayProxyRequestEvent requestEvent = new APIGatewayProxyRequestEvent();
+    APIGatewayProxyResponseEvent responseEvent = getAllAlbumsHandler.handleRequest(requestEvent, context);
+
+    // 驗證狀態碼和其他預期行為
+    assertEquals(200, responseEvent.getStatusCode());
+    // 在此添加其他檢查，例如，確保返回的JSON包含正確的專輯和歌曲列表等
+}
+
 }
