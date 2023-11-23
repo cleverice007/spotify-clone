89bac15a14d4ca40c122a71656ebef6b73814fcd
cleverice007 <cleverice90128@gmail.com>
2023-11-23 13:16:21 +0800

    刪除repo 資料夾
    lambda 不能使用jpa

--- sam-spotify-clone/HelloWorldFunction/src/main/java/com/spotifyclone/demospotifyclone/repo/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spotifyclone.demospotifyclone.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.spotifyclone.demospotifyclone.model.Song;
-
-public interface SongRepository extends JpaRepository<Song, String> {
-}
